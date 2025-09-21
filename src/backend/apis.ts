08b9c85abf136f9e6bddaff4ac2d4363fac4b1ce
calebomondi <kalebmokua@gmail.com>
2025-09-21 17:46:09 +0300

    created

---
+++ src/backend/apis.ts
@@ -0,0 +1,25 @@
+import axios from 'axios'
+import type { AxiosResponse } from 'axios'
+import type { Stats } from '../types/index.types'
+
+const API_URL = 'http://localhost:3000'
+
+/* api call for platform stats */
+
+export const getPlatformStats = async (): Promise<Stats> => {
+    try {
+        const response: AxiosResponse<Stats> = await axios.get(
+            `${API_URL}/api/stats`,
+            {
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }
+        );
+
+        return response.data;
+    } catch (error) {
+        console.error(`Failed to fetch platform stats: ${error}`)
+        throw new Error(`Failed to fetch platform stats.`)
+    }
+}
---
+++ src/types/index.types.ts
@@ -0,0 +1,8 @@
+export interface Stats {
+    total_amount_deposited: number;
+    total_users: number;
+    total_vaults: number;
+    average_deposit_per_user: number;
+    top_depositor: string;
+    top_depositor_amount: number;
+}
