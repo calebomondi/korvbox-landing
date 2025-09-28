8a8f757cc63fa17bc5a5617e1516b2fdd3327f1f
calebomondi <kalebmokua@gmail.com>
2025-09-28 20:36:59 +0300

    added routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import HomePage from './pages/HomePage'
+import MonkeyTreeLeaderboard from './pages/gamify'
+import PrivacyPolicy from './pages/PrivacyPolicy'
+import Terms from './pages/Terms'
 
 function App() {
   return (
     <>
       <Router>
         <Routes>
-          <Route path="/" element={<HomePage />} />          
+          <Route path="/" element={<HomePage />} />
+          <Route path="/game" element={<MonkeyTreeLeaderboard />} />    
+          <Route path="/privacy-policy" element={<PrivacyPolicy />} />  
+          <Route path="/terms-of-use" element={<Terms />} />  
         </Routes>
       </Router>
     </>
