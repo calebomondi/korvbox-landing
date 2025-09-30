8a102e95c2b7157a9acff316e47c364dc54ee998
calebomondi <kalebmokua@gmail.com>
2025-09-30 13:13:17 +0300

    readjusted paths

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import MonkeyTreeLeaderboard from './pages/gamify'
-import PrivacyPolicy from './pages/PrivacyPolicy'
-import Terms from './pages/Terms'
+import PrivacyPolicy from './pages/legal/PrivacyPolicy'
+import Terms from './pages/legal/Terms'
 
 function App() {
   return (
