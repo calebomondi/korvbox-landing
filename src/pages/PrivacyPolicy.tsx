1142f218e71228370851bec063afffae6eaff04b
calebomondi <kalebmokua@gmail.com>
2025-09-30 13:12:37 +0300

    moved

--- src/pages/PrivacyPolicy.tsx
+++
@@ -1,7 +0,0 @@
-//
-
-export default function PrivacyPolicy() {
-  return (
-    <div>PrivacyPolicy</div>
-  )
-}
--- src/pages/Terms.tsx
+++
@@ -1,7 +0,0 @@
-//
-
-export default function Terms() {
-  return (
-    <div>Terms</div>
-  )
-}
