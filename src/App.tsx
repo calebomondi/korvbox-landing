f7173c150f48ddfb9942c5936fe56e1e37d0bc7d
calebomondi <kalebmokua@gmail.com>
2025-10-22 14:39:17 +0300

    add route for Audit page in App component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import PrivacyPolicy from './pages/legal/PrivacyPolicy'
 import Terms from './pages/legal/Terms'
+import Audit from './pages/Audit'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
           <Route path="/" element={<HomePage />} />  
           <Route path="/privacy-policy" element={<PrivacyPolicy />} />  
           <Route path="/terms-of-use" element={<Terms />} />  
+          <Route path="/audit" element={<Audit />} />  
         </Routes>
       </Router>
     </>
